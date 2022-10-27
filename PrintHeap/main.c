b694e87b6845fbfe27a85f3b0ef560d24b06b1f8
tomisawa <tomisawa@maebashi-it.ac.jp>
2022-10-27 21:11:28 +0900

    完全2分木にしてみた．

--- PrintHeap/main.c
+++ PrintHeap/main.c
@@ -9,10 +9,10 @@
 #include <stdio.h>
 #include <math.h>
 #include "PrintHeap.h"
-#define LINE_WIDTH 70
+#define LINE_WIDTH 90
 
 int main() {
-  int tree[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5 ,6,7,8 ,9};
+  int tree[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5 ,6,7,8 ,9,8,7,6,5,4,3,2,1,0,9,8,7};
   PrintHeap(tree, sizeof(tree)/sizeof(int), LINE_WIDTH);
 
   return 0;
