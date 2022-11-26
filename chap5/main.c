f5c68de25aa6c255f6e7cb0f8b683ba00d9aa0b0
tomisawa <tomisawa@maebashi-it.ac.jp>
2022-11-26 13:05:39 +0900

    SimplestSort関数の追加

--- chap5/main.c
+++ chap5/main.c
@@ -6,7 +6,7 @@
 //  Copyright (c) 2022 Masaki Tomisawa. All rights reserved.
 //
 // Algorithm 5.1 SelectionSort(int D[], int size)
-// Algorithm 5.2 InsertionSort(int D[], int size) 
+// Algorithm 5.2 InsertionSort(int D[], int size)
 //
 
 #include <stdio.h>
@@ -20,19 +20,16 @@ void swap(int *a, int *b) {
   temp = *a;
   *a = *b;
   *b = temp;
-  return;
 }
 
 // Algorithm 5.1
 void SelectionSort(int D[], int size) {
   int max, max_index;
-
   for (int i = size - 1; i > 0; i = i - 1) {
-    max_index = 0;  // D [0] is the initial maximum value．.
+    max_index = 0;       // D [0] is the initial maximum value．.
     max = D[max_index];
-
     for (int j = 1; j <= i; j = j + 1) {
-      if (D[j] >= max) { // >= or > 
+      if (D[j] >= max) { // >= or >
         max_index = j;
         max = D[max_index];
       }
@@ -40,8 +37,6 @@ void SelectionSort(int D[], int size) {
 
     swap(&D[max_index], &D[i]);
   }
-
-  return;
 }
 
 // Algorithm 5.2
@@ -49,30 +44,45 @@ void InsertionSort(int D[], int size) {
   for (int i = 1; i < size; i = i + 1) {
     int x = D[i];
     int j = i;
-
     while (D[j - 1] > x && j > 0) {
       D[j] = D[j - 1];
       j = j - 1;
     }
     D[j] = x;
   }
+}
 
-  return;
+//
+// https://arxiv.org/abs/2110.01111
+//
+void SimplestSort(int D[], int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (D[i] < D[j]) { swap(&D[i], &D[j]); }
+    }
+  }
 }
 
 int main(void) {
   int D[21];
   int size;
-
+  
   size = sizeof(D) / sizeof(D[0]);
-
+  
   initData(D, size);
   PrintD(D, size);
   InsertionSort(D, size);
   PrintD(D, size);
-
+  
   initData(D, size);
   PrintD(D, size);
   SelectionSort(D, size);
   PrintD(D, size);
+
+  initData(D, size);
+  PrintD(D, size);
+  SimplestSort(D, size);
+  PrintD(D, size);
+
+  
 }
