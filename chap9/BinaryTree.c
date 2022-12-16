b798ddac55aee4c3077c23d2fae94b6794b9a593
tomisawa <tomisawa@maebashi-it.ac.jp>
2022-12-16 22:56:28 +0900

    setmpの処理を変更した．

--- chap9/BinaryTree.c
+++ chap9/BinaryTree.c
@@ -23,7 +23,7 @@ void PrintM(int M[], int n)
   printf("\n");
 }
 
-int BinaryTree(int level, int Y[], const int b, const int n)
+void BinaryTree(int level, int Y[], const int b, const int n)
 {
   if (level > n) {
     int sum = 0;
@@ -36,18 +36,20 @@ int BinaryTree(int level, int Y[], const int b, const int n)
     Y[level] = 0; BinaryTree(level + 1, Y, b,  n);
     Y[level] = 1; BinaryTree(level + 1, Y, b,  n);
   }
-  if (level == 1) { return -1; }
-  return 0;
+  if (level == 1) printf("End of Recursive Call\n");
 }
 
 int main(void)
 {
   int Y[] = { 0, 0, 0, 0 };      // Y[0] is not used.
   int size = COUNTOF(Y);
-  int yes;
   BinaryTree(1, Y, 2, size - 1); // n is size-1.
-  //  if (!setjmp(env)) yes = BinaryTree(1, Y, 2, size - 1); // n is size-1.
-  //  if (yes != 0) { printf("NO\n"); } else { printf("YES\n"); }
-
-  return 0;
+  //  if (!setjmp(env))
+  //  {
+  //    BinaryTree(1, Y, 3, size - 1); // n is size-1.
+  //    printf("NO\n");
+  //  }
+  //  else {
+  //    printf("YES\n");
+  //  }
 }
