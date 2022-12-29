918817bd8b27b0e24f15633b5d6a6866a70176d2
tomisawa <tomisawa@maebashi-it.ac.jp>
2022-12-29 20:58:06 +0900

    大域変数の扱いを変えた．

--- chap9/a9-3a.c
+++ chap9/a9-3a.c
@@ -21,26 +21,20 @@ typedef struct {
   int  v;
 } PTable;
 
-//  global variable.
+//  static global variable.
 static  PTable T91[N_PAINTING];
-static int     n;  // Problem size
-static int    *X;  // [N_PAINTING];
-static int    *ZX; // [N_PAINTING];
-static int     z;  // Value of provisional solution
-static int     c;  // Knapsack capacity
 
 int ReadPTable(char *fname) {
   FILE *fp;
-
   // const char *fname = "knap2.cvs";
-  if ((fp = fopen(fname, "r")) == NULL) {
+  if((fp = fopen(fname, "r")) == NULL ) {
     printf("Not open %s\n", fname);
     exit(EXIT_FAILURE);
   }
 
   int i = 0;
-  while ((fscanf(fp, "%d,%[^,],%d,%d", &(T91[i].Id), T91[i].Name, &(T91[i].w),
-                 &(T91[i].p))) != EOF) {
+  while((fscanf(fp, "%d,%[^,],%d,%d", &(T91[i].Id), T91[i].Name, &(T91[i].w),
+                &(T91[i].p))) != EOF ) {
     T91[i].v = T91[i].p / T91[i].w;
     i = i + 1;
   }
@@ -49,7 +43,7 @@ int ReadPTable(char *fname) {
 }
 
 void PrintPTable(void) {
-  for (int i = 0; i < n; i++) {
+  for( int i = 0; i < COUNTOF(T91); i++ ) {
     printf("(%d) P-%d, %2s, %2d[kg], %2d[YEN],%2d[YEN/Kg]\n",
            i,
            T91[i].Id,
@@ -61,9 +55,16 @@ void PrintPTable(void) {
   printf("\n");
 }
 
+//  static global variable.
+static int     n;  // Problem size
+static int    *X;  // [N_PAINTING];
+static int    *ZX; // [N_PAINTING];
+static int     z;  // Value of provisional solution
+static int     c;  // Knapsack capacity
+
 void SumWP(int *w, int *v) {
   *w = *v = 0;
-  for (int i = 0; i < n; i++) {
+  for( int i = 0; i < n; i++ ) {
     *w += T91[i].w * X[i];
     *v += T91[i].p * X[i];
   }
@@ -71,8 +72,7 @@ void SumWP(int *w, int *v) {
 
 int Vmax(int level) {
   int Vmax = 0;
-
-  for (int i = level; i < n; i++) { if (T91[i].v > Vmax) { Vmax = T91[i].v; } }
+  for( int i = level; i < n; i++ ) { if( T91[i].v > Vmax ) { Vmax = T91[i].v; } }
   return Vmax;
 }
 
@@ -82,22 +82,24 @@ int Vmax(int level) {
 
 void BB_01knapsack(int level) {
   int w, v;
-
   SumWP(&w, &v);
   
-  if (level >= n) {
-    if ((w <= c) && (v > z)) {
+  if( level >= n ) {
+    if((w <= c) && (v > z)) {
       z = v;
       memcpy(ZX, X, sizeof(ZX) * n);
+      printf("leaf level=%d,w=%d,v=%d,z=%d\n",level, w,v,z);
     }
   } else {
-    if (!CheckA) {
-      if (CheckB) {
-        if (v > z) {
+    printf("level=%d,w=%d,v=%d,z=%d\n",level,w,v,z);
+    if( !CheckA ) {
+      if( CheckB ) {
+        if( v > z ) {
           z = v;
           memcpy(ZX, X, sizeof(ZX) * n);
+          printf("max z=%d\n",z);
         }
-      } else if (!CheckC) {
+      } else if( !CheckC ) {
         X[level] = 1; BB_01knapsack(level + 1);
         X[level] = 0; BB_01knapsack(level + 1);
       }
@@ -116,11 +118,11 @@ int main(void) {
   BB_01knapsack(0);
 
   int sump = 0, sumw = 0;
-  for (int i = 0; i < n; i++) {
+  for( int i = 0; i < n; i++ ) {
     sump += ZX[i] * T91[i].p;
     sumw += ZX[i] * T91[i].w;
   }
   printf("\n");
-  for (int i = 0; i < n; i++) { printf("%d", ZX[i]); }
+  for( int i = 0; i < n; i++ ) { printf("%d", ZX[i]); }
   printf("\tPrice=%d\tWeight=%d\n", sump, sumw);
 }
